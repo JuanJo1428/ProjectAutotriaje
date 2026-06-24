84f2104c68beb4aa6b4a48502229c17c8bacbed4
JuanJo1428 <juvasquezco@unal.edu.co>
2026-06-24 12:25:29 -0500

    Projects in class library .net framework

---
+++ ProjectData/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using Project.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Paciente> Pacientes { get; set; }
+
+        public DbSet<Atencion> Atenciones { get; set; }
+    }
+}
---
+++ ProjectData/Entities/RegistroAtencion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Data.Entities
+{
+    [Table("RegistrosAtencion")]
+    public class Atencion
+    {
+        [Key]
+        public int IdAtencion { get; set; }
+
+        public int IdPaciente { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+
+        public int EdadPaciente { get; set; }
+
+        public string MotivoConsulta { get; set; }
+
+        public bool CondicionEdad { get; set; }
+
+        public bool CondicionMatenidad { get; set; }
+
+        public bool CondicionMental { get; set; }
+
+        public bool CondicionOncologica { get; set; }
+    }
+}
