cd37a27b2f28c3c964eca64c61c79aed9a7c9283
JuanJo1428 <juvasquezco@unal.edu.co>
2026-06-25 15:10:08 -0500

    174023 -- Creacion ITipoDocumentoRepository y sus respetivos metodos

--- ProjectData/Repositories/Interfaces/ITipoDocumentoRepository.cs
+++ ProjectData/Repositories/Interfaces/ITipoDocumentoRepository.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using ProjectData.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectData.Repositories.Interfaces
+{
+    public interface ITipoDocumentoRepository
+    {
+        List<TipoDocumento> ObtenerTodos();
+
+        string ObtenerNombre(int idTipoDocumento);
+
+        string ObtenerDescripcion(int idTipoDocumento);
+
+        TipoDocumento ObtenerPorId(int idTipoDocumento);
+    }
+}
