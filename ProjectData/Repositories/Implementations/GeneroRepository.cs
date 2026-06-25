01306ab3976f76e41b917285c3d7a0f3074749a3
Richi <rfuentesa@unal.edu.co>
2026-06-25 18:38:59 -0500

    174024 Definición de métodos

--- ProjectData/Repositories/Implementations/GeneroRepository.cs
+++ ProjectData/Repositories/Implementations/GeneroRepository.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using ProjectData.Repositories.Implementations;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectData.Repositories.Interfaces;
+using ProjectData.Entities;
 
 namespace ProjectData.Repositories.Implementations
 {
-    internal class GeneroRepository
+    internal class GeneroRepository : IGeneroRepository
     {
+        private readonly DbContext _context;
+
+        public GeneroRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Genero> ObtenerTodos()
+        {
+            return _context.Set<Genero>().ToList();
+        }
+
+        public int ObtenerId(string descripcion)
+        {
+            var genero = _context.Set<Genero>().FirstOrDefault(g => g.Descripcion == descripcion);
+            return genero != null ? genero.IdGenero : 0;
+        }
+
+        public string ObtenerDescripcion(int idGenero)
+        {
+            var genero = _context.Set<Genero>().FirstOrDefault(g => g.IdGenero == idGenero);
+            return genero != null ? genero.Descripcion : null;
+        }
     }
 }
