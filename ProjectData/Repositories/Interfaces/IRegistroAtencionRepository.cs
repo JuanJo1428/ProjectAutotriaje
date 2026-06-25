1c23f5c5b71c57eb2ada2372dcf811e6186f08bc
JuanJo1428 <juvasquezco@unal.edu.co>
2026-06-25 15:06:30 -0500

    174021 -- Creacion IRegistroAtencionRepository y sus respectivos metodos

--- ProjectData/Repositories/Interfaces/IRegistroAtencionRepository.cs
+++ ProjectData/Repositories/Interfaces/IRegistroAtencionRepository.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using Project.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectData.Repositories.Interfaces
+{
+    public interface IRegistroAtencionRepository
+    {
+        RegistroAtencion CrearRegistro(RegistroAtencion atencion);
+
+        RegistroAtencion ActualizarRegistro(RegistroAtencion registro);
+
+        bool EliminarRegistro(int idRegistro);
+
+        RegistroAtencion ObtenerPorId(int idRegistro);
+
+        List<RegistroAtencion> ObtenerRegistrosPorPaciente(int idPaciente);
+
+        List<RegistroAtencion> ObtenerTodos();
+
+    }
+}
