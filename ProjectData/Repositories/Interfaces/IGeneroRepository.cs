a0a47e54c193877d654e948045ca08aad21c1971
JuanJo1428 <juvasquezco@unal.edu.co>
2026-06-25 15:05:05 -0500

    174025 -- Creacion IGeneroRepository con sus respectivos metodos

---
+++ ProjectData/Repositories/Interfaces/IGeneroRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProjectData.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectData.Repositories.Interfaces
+{
+    public interface IGeneroRepository
+    {
+        List<Genero> ObtenerTodos();
+
+        int ObtenerId(string descripcion);
+
+        string ObtenerDescripcion(int idGenero);
+
+    }
+}
