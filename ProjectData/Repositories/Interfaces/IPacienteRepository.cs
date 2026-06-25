cedeb4857e7398bd59eb8a0cf72db5a53df4ef19
JuanJo1428 <juvasquezco@unal.edu.co>
2026-06-25 15:20:07 -0500

    174019 -- Creacion IPacienteRepository y sus respectivos metodos

--- ProjectData/Repositories/Interfaces/IPacienteRepository.cs
+++ ProjectData/Repositories/Interfaces/IPacienteRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,17 @@ namespace ProjectData.Repositories.Interfaces
 {
     public interface IPacienteRepository
     {
+        Paciente CrearPaciente(Paciente paciente);
 
+        Paciente ActualizarPaciente(Paciente paciente);
+
+        bool InactivarPaciente(int idPaciente);
+
+        Paciente ObtenerPorId(int idPaciente);
+
+        Paciente ObtenerPorDocumento(int idTipoDocumento, string nroDocumento);
+
+        List<Paciente> ObtenerTodos();
 
     }
 }
