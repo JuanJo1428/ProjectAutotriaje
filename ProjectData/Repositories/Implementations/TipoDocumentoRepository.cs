8b5e53b1e6af575092f0dd49e40d52acdbdb92ab
Richi <rfuentesa@unal.edu.co>
2026-06-25 19:24:01 -0500

    174022 Creacion de métodos tipo documento repository

--- ProjectData/Repositories/Implementations/TipoDocumentoRepository.cs
+++ ProjectData/Repositories/Implementations/TipoDocumentoRepository.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using ProjectData.Repositories.Implementations;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectData.Repositories.Interfaces;
+using ProjectData.Entities;
 
 namespace ProjectData.Repositories.Implementations
 {
-    internal class TipoDocumentoRepository
+    internal class TipoDocumentoRepository : ITipoDocumentoRepository
     {
+        private readonly DbContext _context;
+
+        public TipoDocumentoRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public List<TipoDocumento> ObtenerTodos()
+        {
+            return _context.Set<TipoDocumento>().ToList();
+        }
+
+        public int ObtenerId(string descripcion)
+        {
+            var tipoDocumento = _context.Set<TipoDocumento>().FirstOrDefault(td => td.Descripcion == descripcion);
+            return tipoDocumento != null ? tipoDocumento.IdTipoDocumento : 0;
+        }
+
+        public string ObtenerNombre(int idTipoDocumento)
+        {
+            var tipoDocumento = _context.Set<TipoDocumento>().FirstOrDefault(td => td.IdTipoDocumento == idTipoDocumento);
+            return tipoDocumento != null ? tipoDocumento.Nombre : null;
+        }
+
+        public string ObtenerDescripcion(int idTipoDocumento)
+        {
+            var tipoDocumento = _context.Set<TipoDocumento>().FirstOrDefault(td => td.IdTipoDocumento == idTipoDocumento);
+            return tipoDocumento != null ? tipoDocumento.Descripcion : null;
+        }
+
+        public TipoDocumento ObtenerPorId(int idTipoDocumento)
+        {
+            return _context.Set<TipoDocumento>().FirstOrDefault(td => td.IdTipoDocumento == idTipoDocumento);
+        }
     }
 }
